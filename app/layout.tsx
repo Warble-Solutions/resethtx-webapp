0d09e2d5cd010c53d98cd38048a05756ff871149
Tushar Chauhan <tusharchauhan3825@gmail.com>
2026-02-05 05:26:53 +0530

    feat: integrate Google Analytics 4 via @next/third-parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer'
 import FloatingSocials from './components/FloatingSocials'
@@ -68,6 +69,7 @@ export default function RootLayout({
           </InquireProvider>
         </ReservationProvider>
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
     </html>
   );
 }
